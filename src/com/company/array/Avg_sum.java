16724b90b7a88d461b0e4749efd74035df3a9fb6
arata-bhuyan <aratabhuyan360@gmail.com>
2024-03-05 23:31:29 +0530

    Avg&sum of array

---
+++ src/com/company/array/Avg_sum.java
@@ -0,0 +1,26 @@
+package com.company.array;
+
+import java.util.Scanner;
+
+public class Avg_sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[3][2];
+        int avg = 0;
+        int sum = 0;
+        System.out.println("Enter your array element: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The sum of the element is :");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+                sum = sum + arr[i][j];
+            }
+        }
+        System.out.println("The sum of element is " + sum);
+        System.out.println("The average is" + (sum/(3*2)));
+    }
+}
