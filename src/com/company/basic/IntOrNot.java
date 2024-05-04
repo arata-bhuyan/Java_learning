42ce4e4b01c716cde25fbe662039fcb5ad1a6297
arata-bhuyan <aratabhuyan360@gmail.com>
2024-05-04 13:33:50 +0530

    add

--- src/com/company/basic/IntOrNot.java
+++ src/com/company/basic/IntOrNot.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.basic;
 
 import java.util.Scanner;
 
--- src/com/company/basic/Operat.java
+++ src/com/company/basic/Operat.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.basic;
 
 public class Operat {
     public static void main(String[] args) {
